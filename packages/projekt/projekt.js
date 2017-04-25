2dc69202eda6ac1c049935d80e1c23dfb2ca64ac
Justin Behanna <j.behanna@gmail.com>
2017-04-25 11:20:04 -0400

    added custom package for global functions and settings

---
+++ packages/projekt/projekt.js
@@ -0,0 +1,13 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See projekt-tests.js for an example of importing.
+export const name = 'projekt';
+
+// settings for this message fade
+export const projekt = {
+  messageTransition: {
+    animation: 'fade down',
+    duration: 100,
+  },
+};
